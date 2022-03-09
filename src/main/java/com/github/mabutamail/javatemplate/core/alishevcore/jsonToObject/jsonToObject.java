93904cf2be44048cf21dab1fdb6a1bf2dd7eec5f
mabuta <mabutamail@gmail.com>
2022-03-09 23:02:03 +0300

    json to object

--- src/main/java/com/github/mabutamail/javatemplate/core/alishevcore/jsonToObject/jsonToObject.java
+++ src/main/java/com/github/mabutamail/javatemplate/core/alishevcore/jsonToObject/jsonToObject.java
@@ -1,17 +1,31 @@
 package com.github.mabutamail.javatemplate.core.alishevcore.jsonToObject;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class jsonToObject {
     public static void main(String[] args) {
-//        ObjectMapper mapper = new ObjectMapper();
-        String json = "{\"name\":\"zalex\"}";
-        log.info(json);
-//        Person person = new Person(json);
+        ObjectMapper mapper = new ObjectMapper();
+        String json = "{\"age\":33, \"name\":\"zalex\", \"pass\":true}";
+//        log.info(json);
         // Java объект из JSON строки
-//        Person personFromString = new Person(@RequestBody json);
-
+//        Person personFromString = new Person();
+//        personFromString.setName("zalex111");
+//        String result = null;
+//        try {
+//            result = mapper.writeValueAsString(personFromString);
+//        } catch (JsonProcessingException e) {
+//            e.printStackTrace();
+//        }
+//        log.info(result);
+        try {
+            Person personFromString = mapper.readValue(json, Person.class);
+            log.info(String.valueOf(personFromString));
+        } catch (JsonProcessingException e) {
+            log.error("не парсится ", e);
+        }
 
     }
 }
