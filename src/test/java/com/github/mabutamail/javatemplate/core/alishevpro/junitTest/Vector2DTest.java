4cf5f68c04bb49b773cb37c433a0f592b78a26cf
mabuta <mabutamail@gmail.com>
2022-04-26 14:58:37 +0300

    alishev junit

--- src/test/java/com/github/mabutamail/javatemplate/core/alishevpro/junitTest/Vector2DTest.java
+++ src/test/java/com/github/mabutamail/javatemplate/core/alishevpro/junitTest/Vector2DTest.java
@@ -1,31 +1,40 @@
 package com.github.mabutamail.javatemplate.core.alishevpro.junitTest;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class Vector2DTest {
 
+    private static final double DELTA = 1e-9;
+    private static Vector2D v1;
+
+    @Before
+    public void createNewVector() {
+         v1 = new Vector2D();   //  action
+    }
+
     @Test
     public void newVectorShouldHaveZeroLength() {
-        Vector2D v1 = new Vector2D();   //  action
+//        Vector2D v1 = new Vector2D();   //  action
         //  assertion
-        Assert.assertEquals(0, v1.length(), 1e-9);
+        Assert.assertEquals(0, v1.length(), DELTA);
     }
 
     @Test
     public void newVectorShouldHaveZeroX() {
-        Vector2D v1 = new Vector2D();   //  action
+//        Vector2D v1 = new Vector2D();   //  action
         v1.setX(0);
         v1.setY(2);
         //  assertion
-        Assert.assertEquals(Math.sqrt(4), v1.length(), 1e-9);
+        Assert.assertEquals(Math.sqrt(4), v1.length(), DELTA);
     }
 
     @Test
     public void newVectorShouldHaveZeroY() {
-        Vector2D v1 = new Vector2D();   //  action
+//        Vector2D v1 = new Vector2D();   //  action
         //  assertion
-        Assert.assertEquals(0, v1.getY(), 1e-9);
+        Assert.assertEquals(0, v1.getY(), DELTA);
     }
 
 }
