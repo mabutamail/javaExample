b7871474c73cdfb0dc0d02824eab6bcae243bee4
alexanderz <mabutamail@gmail.com>
2022-07-12 20:10:19 +0300

    Test WebSocket APIs With Postman

---
+++ src/main/java/com/github/mabutamail/javatemplate/actual/websocket/websockets/Message.java
@@ -0,0 +1,15 @@
+package com.github.mabutamail.javatemplate.actual.websocket.websockets;
+
+public class Message {
+
+    private String from;
+    private String text;
+
+    public String getText() {
+        return text;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+}
